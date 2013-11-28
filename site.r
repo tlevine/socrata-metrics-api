e32d4488aa82e1995af860c65b32bd453cc3a208
Thomas Levine <occurrence@thomaslevine.com>
2013-11-28 21:12:49 +0000

    full combinations of this check

--- site.r
+++ site.r
@@ -7,5 +7,6 @@ if (!('site' %in% ls())) {
 col.names <- list(total = cset(colnames(site)[grepl('.total$', colnames(site))]))
 col.names$change <- cset_intersection(cset(sub('.total$','',col.names$total)), colnames(site))
 col.names$total.but.no.change <- cset_difference(cset(sub('.total$','',col.names$total)), col.names$change)
-col.names$other <- cset_difference(cset_union(col.names$total, col.names$change, col.names$total.but.no.change), colnames(site))
+col.names$change.but.no.total <- cset_difference(col.names$change, cset(sub('.total$','',col.names$total)))
+col.names$other <- cset_difference(colnames(site), cset_union(col.names$total, col.names$change, col.names$total.but.no.change))
 # ratings.count might be the change for ratings.total
